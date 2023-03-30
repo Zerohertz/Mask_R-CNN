2312e19a57f73bcfb5a1b74d5c0b2146efe60562
Zerohertz <ohg3417@gmail.com>
2023-03-30 16:13:30 +0900

    :hammer: Update: train

--- train.py
+++ train.py
@@ -2,28 +2,15 @@ import torch
 
 from model import *
 
-from utils import utils
-from utils import get_transform, CustomizedDataset
-
 
 def main():
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     ##### Prepare Dataset #####
-    TrainingDataset = CustomizedDataset("../data/TrainingData", get_transform(train=True))
-    TestDataset = CustomizedDataset("../data/TrainingData", get_transform(train=False))
-
-    indices = torch.randperm(len(TrainingDataset)).tolist()
-    TrainingDataset = torch.utils.data.Subset(TrainingDataset, indices[:-50])
-    TestDataset = torch.utils.data.Subset(TestDataset, indices[-50:])
-
-    TrainingDataset = torch.utils.data.DataLoader(
-        TrainingDataset, batch_size=2, shuffle=True, num_workers=4,
-        collate_fn=utils.collate_fn)
-
-    TestDataset = torch.utils.data.DataLoader(
-        TestDataset, batch_size=1, shuffle=False, num_workers=4,
-        collate_fn=utils.collate_fn)
+    TrainingDataset, TestDataset = load_data("../data/TrainingData",
+                                             "../data/TestData",
+                                             batch_size=8,
+                                             num_workers=16)
 
     ##### INIT Mask R-CNN #####
     num_classes = 3
