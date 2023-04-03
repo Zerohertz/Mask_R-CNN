522288d6e2825ae18a986f87e4f78c946851d14b
Zerohertz <ohg3417@gmail.com>
2023-04-03 13:17:02 +0900

    :memo: Update: Draw Ground Truth

--- model/test.py
+++ model/test.py
@@ -12,6 +12,38 @@ from tqdm import tqdm
 from utils import get_transform, CustomizedDataset
 
 
+def draw_gt(CD, obj):
+    if not 'exp' in os.listdir():
+        os.mkdir('exp')
+    if not 'Ground_Truth' in os.listdir('exp'):
+        os.mkdir('exp/Ground_Truth')
+    else:
+        shutil.rmtree('exp/Ground_Truth')
+        os.mkdir('exp/Ground_Truth')
+    for i, tmp in enumerate(tqdm(CD)):
+        img, boxes, masks, labels = tmp[0], tmp[1]['boxes'], tmp[1]['masks'], tmp[1]['labels']
+        for box, mask, label in zip(boxes, masks, labels):
+            box, mask, label = box.numpy(), mask.numpy(), int(label)
+            try:
+                label = obj[label]
+            except:
+                label = 'Unknown: ' + str(label)
+            img = img.permute(1,2,0)
+            img = img.numpy() * 255.0
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            color = (random.randrange(0,256),random.randrange(0,256),random.randrange(0,256))
+            thickness = 1
+            x1, y1, x2, y2 = box.astype(int)
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, thickness)
+            label_text = f"{label}"
+            cv2.putText(img, label_text, (x1, y1-5), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color, thickness)
+            mask = (mask > 0.5)
+            masked_img = cv2.bitwise_and(img, img, mask=mask.astype(np.uint8))
+            no_masked_img = cv2.bitwise_and(img, img, mask=255-mask.astype(np.uint8))
+            masked_img[np.where((masked_img != [0, 0, 0]).all(axis=2))] = color
+            img = cv2.addWeighted(masked_img, 0.5, no_masked_img, 1, 0)
+        cv2.imwrite('exp/Ground_Truth/' + CD.imgs[i], img)
+
 def init_output(output):
     boxes = output[0]['boxes'].cpu().detach().numpy()
     scores = output[0]['scores'].cpu().detach().numpy()
